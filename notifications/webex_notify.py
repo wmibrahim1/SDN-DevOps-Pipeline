10a65afc933500526c37d18ffa697a52d097a8c4
wmibrahim1 <wmibrahim@cpp.edu>
2025-11-30 18:03:35 -0800

    Update webex_notify.py

--- notifications/webex_notify.py
+++ notifications/webex_notify.py
@@ -4,7 +4,7 @@ import requests
 import os
 
 WEBEX_TOKEN = os.getenv("MmFmYjI5ODItZTg0NS00YjQ2LTliMTMtZTM0MjVhMTYxMzJlYzE4MDk1NDUtMTM2_P0A1_13494cac-24b4-4f89-8247-193cc92a7636")
-WEBEX_ROOM_ID = os.getenv("aHR0cHM6Ly9jb252LXIud2J4Mi5jb20vY29udmVyc2F0aW9uL2FwaS92MS9jb252ZXJzYXRpb25zLzhjMjc5NzMwLWM5YWYtMTFmMC04NzYzLThiZDY2MDdhZGVlMg")
+WEBEX_ROOM_ID = os.getenv("aHR0cHM6Ly9jb252LXIud2J4Mi5jb20vY29udmVyc2F0aW9uL2FwaS92MS9jb252ZXJzYXRpb25zLzhjMjc5NzMwLWM5YWYtMTFmMC04NzYzLThiZDY2MDdhZGVlMg==")
 
 def send_message(text):
     url = "https://webexapis.com/v1/messages"
